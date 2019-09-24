c259effb8a045b6660d73935554b314974e983dc
Overlord <1075909452@qq.com>
2019-09-24 13:32:27 +0800

    2019-09-24 目标词表更新程序优化，增加外部读入删减词表功能

--- targetFileProcessing.py
+++ targetFileProcessing.py
@@ -4,33 +4,28 @@
 """
 import pandas as pd
 import pickle
-
-data = pd.read_csv('input/汽车品牌知识库.csv', encoding = 'gb2312',index_col='序号')
-temp0 = data['品牌'].tolist()
-temp0 = list(set(temp0))
-temp0.remove('北京')
-temp0.remove('上海')
-temp0.remove('谷歌')
-aa = []
-for item in temp0:
-    if len(item)==2:
-        aa.append(item+'车')
-temp0 = temp0+aa
-temp1 = data['品牌分类'].tolist()
-temp2 = data['汽车型号'].tolist()
-temp = temp0+temp1+temp2
-data_list = (list(set(temp)))
-# print(len(data_list))
-data_list.append('汽车')
-data_list.append('新能源汽车')
-data_list.append('梅赛德斯')
-data_list.append('北汽')
-data_list.append('一汽')
-data_list.append('上汽')
-data_list.append('广汽')
-data_list.append('福汽')
-data_list.append('一汽大众')
-data_list  = list(map(lambda x:x.upper(),data_list))
-print(len(data_list))
-with open('targets.txt', 'wb') as f:
-    pickle.dump(data_list, f, pickle.HIGHEST_PROTOCOL)
+def targetFileProcessing():
+    data = pd.read_csv('input/汽车品牌知识库.csv', encoding = 'gb2312',index_col='序号')
+    temp0 = data['品牌'].tolist()
+    temp0 = list(set(temp0))
+    aa = []
+    for item in temp0:
+        if len(item) == 2:
+            aa.append(item + '车')
+    temp0 = temp0+aa
+    temp1 = data['品牌分类'].tolist()
+    temp2 = data['汽车型号'].tolist()
+    temp = temp0+temp1+temp2
+    data_list = (list(set(temp)))
+    with open('target_remove_dicr.txt','rb') as f:
+        item = f.readline()
+        if item:
+            data_list.remove(item)
+    with open('target_add_dicr.txt','rb') as f:
+        item = f.readline()
+        if item:
+            data_list.append(item)
+    data_list  = list(map(lambda x:x.upper(),data_list))
+    print(len(data_list))
+    with open('targets.txt', 'wb') as f:
+        pickle.dump(data_list, f, pickle.HIGHEST_PROTOCOL)
