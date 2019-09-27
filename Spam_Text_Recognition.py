3fd448a212122b49c974b15ec9f373be227551d7
Overlord <1075909452@qq.com>
2019-09-27 10:54:48 +0800

    2019-09-27 add Social_media_fliter function

--- Spam_Text_Recognition.py
+++ Spam_Text_Recognition.py
@@ -5,9 +5,8 @@
 import re,pickle
 import jieba
 import opencc
-# import fool
 from patten_config import patten
-
+import jieba.analyse
 jieba.load_userdict("cut_dict.txt")
 cc = opencc.OpenCC('t2s')
 with open('targets.txt', 'rb') as f:
@@ -23,7 +22,7 @@ def target_fliter(title,content,source):
     seg_dict = dict.fromkeys(targets, 0)
     text = title + content
     word_list =list(jieba.cut(title))
-    # print(word_list)
+    print(word_list)
     if source not in Social_media:
         for item in word_list:
             item = item.upper()
@@ -34,13 +33,16 @@ def target_fliter(title,content,source):
     if label==1:
         if source in Social_media:
             flag = 3
+            new_word_list = []
             for item in patten:
+                obj = re.findall(item,text)
+                new_word_list += obj
                 text = re.compile(item).sub('',text)
         else:
             flag = max(len(text) / 400, 2)
 
         text_list = list(jieba.cut(text))
-        # print(text_list)
+        print(text_list)
         for item in text_list:
             item = item.upper()
             if len(item)>1 and item in seg_dict.keys():
@@ -48,7 +50,24 @@ def target_fliter(title,content,source):
                 target += 1
                 if seg_dict[item]>=flag or target>flag*2:
                     label = 0
+                    print(item)
                     break
     return label
+
+def Social_media_fliter(content):
+    text = content
+    print(jieba.analyse.textrank(text, topK=5, withWeight=False))
+    word_list = list(jieba.cut(text))
+    flag = 2
+    new_word_list = []
+    for item in patten:
+        obj = re.findall(item, text)
+        new_word_list += obj
+        text = re.compile(item).sub('', text)
+    # print(','.join(new_word_list))
+    # print(','.join(new_word_list))
+    print(jieba.analyse.textrank(','.join(new_word_list), topK=5, withWeight=True))
+    print(jieba.analyse.textrank(text, topK=5, withWeight=False))
+    # jieba.analyse.textrank(text)
 
 
